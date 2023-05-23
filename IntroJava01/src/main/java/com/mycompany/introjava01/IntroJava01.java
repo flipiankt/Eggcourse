583241f1c7076f635a3045c2b03fc77ee9351b29
“Francy <francy.hoyos@udea.edu.co>
2023-05-23 18:35:25 -0500

    primer ejercicio JAVA

---
+++ IntroJava01/src/main/java/com/mycompany/introjava01/IntroJava01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.introjava01;
+
+/**
+ *
+ * @author francyhoyos
+ */
+public class IntroJava01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
