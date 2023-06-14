118f7f6c888640c34a86ea8ef3415fa2962084a7
“Francy <francy.hoyos@udea.edu.co>
2023-06-13 19:55:46 -0500

    se agregan ejercciios hasta 5 en POO

--- ejercicio5POO/src/main/java/poo/ejercicio5poo/Ejercicio5POO.java
+++ ejercicio5POO/src/main/java/poo/ejercicio5poo/Ejercicio5POO.java
@@ -1,16 +1,26 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+*/
 
 package poo.ejercicio5poo;
+
+import poo.ejercicio5poo.Servicios.Cuenta;
 
 /**
  *
  * @author francyhoyos
  */
 public class Ejercicio5POO {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Cuenta c1= new Cuenta();
+        c1.crearCuenta();
+        c1.consultarSaldo();
+        c1.consultarDatos();
+        c1.extraccionRapida(100);
+        c1.consultarDatos();
+        c1.retirar(200);
+        c1.consultarSaldo();
+        
     }
 }
---
+++ ejercicio5POO/src/main/java/poo/ejercicio5poo/Servicios/Cuenta.java
@@ -0,0 +1,105 @@
+/*
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+*/
+package poo.ejercicio5poo.Servicios;
+
+import java.util.Scanner;
+
+/**
+ *Realizar una clase llamada Cuenta (bancaria) que debe tener como mínimo los atributos: numeroCuenta (entero), el DNI del cliente (entero largo) y el saldo actual (entero). Las operaciones asociadas a dicha clase son:
+ * a) Constructor por defecto y constructor con DNI, saldo, número de cuenta e interés.
+ * b) Agregar los métodos getters y setters correspondientes
+ * c) Metodo para crear un objeto Cuenta, pidiéndole los datos al usuario.
+ * d) Método ingresar(double ingreso): el método recibe una cantidad de dinero a ingresar y se la sumara a saldo actual.
+ * e) Método retirar(double retiro): el método recibe una cantidad de dinero a retirar y se la restará al saldo actual. Si la cuenta no tiene la cantidad de dinero a retirar, se pondrá el saldo actual en 0.
+ * f) Método extraccionRapida(): le permitirá sacar solo un 20% de su saldo. Validar que el usuario no saque más del 20%.
+ * g) Método consultarSaldo(): permitirá consultar el saldo disponible en la cuenta.
+ * h) Método consultarDatos(): permitirá mostrar todos los datos de la cuenta
+ *
+ * @author francyhoyos
+ */
+public class Cuenta {
+    private int numeroCuenta;
+    private int dni;
+    private int saldo;
+    
+    public Cuenta() {
+    }
+    
+    public Cuenta(int numeroCuenta, int dni, int saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.dni = dni;
+        this.saldo = saldo;
+    }
+    
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+    
+    public int getDni() {
+        return dni;
+    }
+    
+    public int getSaldo() {
+        return saldo;
+    }
+    
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+    
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    public void crearCuenta(){
+        Scanner leer =new Scanner (System.in);
+        System.out.println("Ingrese el numero de cuenta");
+        this.numeroCuenta=leer.nextInt();
+        System.out.println("    i   ngrese su DNI");
+        this.dni=leer.nextInt();
+        System.out.println("Ingrese su saldo");
+        this.saldo=leer.nextInt();
+        
+    }
+    
+    public int ingresar(int ingreso){
+        saldo +=ingreso;
+        return saldo;
+    }
+    
+    public int retirar(int retiro){
+        if (retiro>saldo) {
+            saldo=0;
+        }else{
+            saldo -=retiro;
+            
+        }
+        return saldo;
+    }
+    
+    public int extraccionRapida(int retiro){
+        if (retiro<(saldo*0.2)) {
+            saldo-=retiro;
+            
+        } else{
+            System.out.println("no se puede retirar");
+        }
+        return saldo;
+    }
+    
+    public void consultarSaldo(){
+        System.out.println("El saldo es "+saldo);
+    }
+    
+    public void consultarDatos(){
+        System.out.println("El  numero de cuenta es "+numeroCuenta);
+        System.out.println("El DNI ES "+dni);
+        System.out.println("El saldo es "+saldo);
+    }
+}
